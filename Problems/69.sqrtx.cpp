9629cfc70e10aa274c48b644aa728d32493f259a
adrianandrag18 <66696874+adrianandrag18@users.noreply.github.com>
2024-02-22 07:58:22 +0200

    Add solution for 69.sqrtx.cpp

---
+++ Problems/69.sqrtx.cpp
@@ -0,0 +1,16 @@
+// title: Sqrt(x)
+// timestamp: 2024-02-22 07:58:22
+// problemUrl: https://leetcode.com/problems/sqrtx/
+// memory: 7.3 MB
+// runtime: 0 ms
+
+#include<cmath>
+class Solution {
+public:
+    int mySqrt(int x) {
+        // long long i = 1;
+        // for (; i * i <=x; i++);
+        // return i-1;
+        return sqrt(x);
+    }
+};
