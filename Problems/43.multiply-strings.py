20f66dce675ef3bff7d9976292f7f7f177b1b1c5
adrianandrag18 <66696874+adrianandrag18@users.noreply.github.com>
2025-06-12 21:15:32 +0300

    Add solution for 43.multiply-strings.py

---
+++ Problems/43.multiply-strings.py
@@ -0,0 +1,9 @@
+# title: Multiply Strings
+# timestamp: 2025-06-12 21:15:32
+# problemUrl: https://leetcode.com/problems/multiply-strings/
+# memory: 18 MB
+# runtime: 0 ms
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
